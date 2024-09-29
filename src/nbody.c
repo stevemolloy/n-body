4f95a109cf777f7a738540bbf621ac73adc3a467
Stephen Molloy <sdmolloy@gmail.com>
2024-09-29 12:16:48 +0200

    Finally N-bodies.  Runs slow, but it does run :)

--- src/nbody.c
+++ src/nbody.c
@@ -6,7 +6,7 @@
 #define WIDTH 800
 #define HEIGHT 600
 
-#define NUM_PARTICLES 2
+#define NUM_PARTICLES 3
 #define TRAIL_LENGTH 500
 
 typedef struct {
@@ -30,13 +30,13 @@ int main(void) {
   SetTargetFPS(100);
 
   const long double mass_ratio = 3.0;
-  const long double vel = 2.0;
+  const long double vel = 2.5;
 
   Particle particles[NUM_PARTICLES] = {
     {
       .mass = 10000 * mass_ratio,
       .position = (Vector3d) {GetRenderWidth()/2.0, GetRenderHeight()/2.0, 0},
-      .velocity = (Vector3d) {0, -vel, 0},
+      .velocity = (Vector3d) {0, 0, 0},
       .acceleration = (Vector3d) {0, 0, 0},
     },
     {
@@ -44,31 +44,41 @@ int main(void) {
       .position = (Vector3d) {GetRenderWidth()/2.0 - 200, GetRenderHeight()/2.0, 0},
       .velocity = (Vector3d) {0, vel*mass_ratio, 0},
       .acceleration = (Vector3d) {0, 0, 0},
-    }
+    },
+    {
+      .mass = 10000,
+      .position = (Vector3d) {GetRenderWidth()/2.0 + 200, GetRenderHeight()/2.0, 0},
+      .velocity = (Vector3d) {0, -vel*mass_ratio, 0},
+      .acceleration = (Vector3d) {0, 0, 0},
+    },
   };
   long double acceleration_deltas[NUM_PARTICLES] = {0};
-  Vector3d force_vectors[NUM_PARTICLES] = {0};
-  long double radii[NUM_PARTICLES] = {0};
+  Vector3d force_vectors[NUM_PARTICLES][NUM_PARTICLES] = {0};
+  long double radii[NUM_PARTICLES][NUM_PARTICLES] = {0};
 
-  Vector3d trail[2][TRAIL_LENGTH] = {0};
+  Vector3d trail[NUM_PARTICLES][TRAIL_LENGTH] = {0};
 
   size_t counter = 0;
   while (!WindowShouldClose()) {
-    counter = (counter + 1) % TRAIL_LENGTH;
-    trail[0][counter] = particles[0].position;
-    trail[1][counter] = particles[1].position;
     long double dt = GetFrameTime();
-    
-    force_vectors[0] = VECTOR3D_DIFF(particles[1].position, particles[0].position);
-    force_vectors[1] = VECTOR3D_DIFF(particles[0].position, particles[1].position);
-    radii[0] = VECTOR3D_LENGTH(force_vectors[0]);
-    radii[1] = VECTOR3D_LENGTH(force_vectors[1]);
 
-    acceleration_deltas[0] = particles[1].mass / (radii[0]*radii[0]*radii[0]);
-    acceleration_deltas[1] = particles[0].mass / (radii[1]*radii[1]*radii[1]);
+    counter = (counter + 1) % TRAIL_LENGTH;
+    for (size_t i=0; i<NUM_PARTICLES; i++) {
+      trail[i][counter] = particles[i].position;
+    }
+    
+    for (size_t i=0; i<NUM_PARTICLES; i++) {
+      particles[i].acceleration = (Vector3d) {0};
+      for (size_t j=0; j<NUM_PARTICLES; j++) {
+        if (i == j) continue;
+        force_vectors[i][j] = VECTOR3D_DIFF(particles[j].position, particles[i].position);
+        radii[i][j] = VECTOR3D_LENGTH(force_vectors[i][j]);
+        acceleration_deltas[i] = particles[j].mass / (radii[i][j]*radii[i][j]*radii[i][j]);
+        particles[i].acceleration = VECTOR3D_ADD(particles[i].acceleration, VECTOR3D_SCALE(acceleration_deltas[i], force_vectors[i][j]));
+      }
+    }
 
     for (size_t i=0; i<NUM_PARTICLES; i++) {
-      particles[i].acceleration = VECTOR3D_SCALE(acceleration_deltas[i], force_vectors[i]);
       particles[i].velocity = VECTOR3D_ADD(particles[i].velocity, VECTOR3D_SCALE(dt, particles[i].acceleration));
       particles[i].position = VECTOR3D_ADD(particles[i].position, VECTOR3D_SCALE(dt, particles[i].velocity));
     }
@@ -77,12 +87,13 @@ int main(void) {
     ClearBackground(BLACK);
     // DrawRectangle(0, 0, 100, 20, BLACK);
     DrawFPS(0, 0);
-    for (size_t i=0; i<TRAIL_LENGTH; i++) {
-      DrawCircle(trail[0][i].x, trail[0][i].y, 2, GRAY);
-      DrawCircle(trail[1][i].x, trail[1][i].y, 2, GRAY);
+    for (size_t part=0; part<NUM_PARTICLES; part++) {
+      for (size_t i=0; i<TRAIL_LENGTH; i++) {
+        DrawCircle(trail[part][i].x, trail[part][i].y, 2, GRAY);
+        DrawCircle(trail[part][i].x, trail[part][i].y, 2, GRAY);
+      }
+      DrawCircle(particles[part].position.x, particles[part].position.y, particles[part].mass/5000, WHITE);
     }
-    DrawCircle(particles[0].position.x, particles[0].position.y, particles[0].mass/5000, WHITE);
-    DrawCircle(particles[1].position.x, particles[1].position.y, particles[1].mass/5000, WHITE);
     EndDrawing();
   }
 
