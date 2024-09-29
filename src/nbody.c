2fbec6ec5b6d64919d00ab2f4bd8824c721115da
Stephen Molloy <sdmolloy@gmail.com>
2024-09-29 14:32:08 +0200

    No need for z coord. Added a momentum zeroing function.

--- src/nbody.c
+++ src/nbody.c
@@ -3,60 +3,94 @@
 
 #include "raylib.h"
 
-#define WIDTH 800
-#define HEIGHT 600
+#define WIDTH 1000
+#define HEIGHT 800
 
-#define NUM_PARTICLES 3
+#define NUM_PARTICLES 6
 #define TRAIL_LENGTH 500
 
 typedef struct {
-  long double x, y, z;
-} Vector3d;
+  long double x, y;
+} Vector2d;
 
 typedef struct {
-  Vector3d position;
-  Vector3d velocity;
-  Vector3d acceleration;
+  Vector2d position;
+  Vector2d velocity;
+  Vector2d acceleration;
   long double mass;
 } Particle;
 
-#define VECTOR3D_SCALE(S, V) (Vector3d) { .x=(S)*(V).x, .y=(S)*(V).y, .z=(S)*(V).z }
-#define VECTOR3D_ADD(A, B)   (Vector3d) { .x=(A).x+(B).x, .y=(A).y+(B).y, .z=(A).z+(B).z}
-#define VECTOR3D_DIFF(A, B)  (Vector3d) { .x=(A).x-(B).x, .y=(A).y-(B).y, .z=(A).z-(B).z}
-#define VECTOR3D_LENGTH(V)   sqrtf((V).x*(V).x + (V).y*(V).y + (V).z*(V).z)
+#define VECTOR3D_SCALE(S, V) (Vector2d) { .x=(S)*(V).x, .y=(S)*(V).y }
+#define VECTOR3D_ADD(A, B)   (Vector2d) { .x=(A).x+(B).x, .y=(A).y+(B).y }
+#define VECTOR3D_DIFF(A, B)  (Vector2d) { .x=(A).x-(B).x, .y=(A).y-(B).y }
+#define VECTOR3D_LENGTH(V)   sqrtf((V).x*(V).x + (V).y*(V).y)
+
+void zero_momentum(Particle *particles, size_t count) {
+  long double x_momentum = 0, y_momentum = 0;
+  long double total_mass = 0;
+
+  for (size_t i=0; i<count; i++) {
+    x_momentum += particles[i].velocity.x * particles[i].mass;
+    y_momentum += particles[i].velocity.y * particles[i].mass;
+    total_mass += particles[i].mass;
+  }
+
+  for (size_t i=0; i<count; i++) {
+    particles[i].velocity.x -= x_momentum / total_mass;
+    particles[i].velocity.y -= y_momentum / total_mass;
+  }
+}
 
 int main(void) {
   InitWindow(WIDTH, HEIGHT, "N-body problem");
   SetTargetFPS(100);
 
-  const long double mass_ratio = 3.0;
-  const long double vel = 2.5;
-
+  float part_rad[NUM_PARTICLES] = {10, 2, 2, 2, 2, 2};
   Particle particles[NUM_PARTICLES] = {
     {
-      .mass = 10000 * mass_ratio,
-      .position = (Vector3d) {GetRenderWidth()/2.0, GetRenderHeight()/2.0, 0},
-      .velocity = (Vector3d) {0, 0, 0},
-      .acceleration = (Vector3d) {0, 0, 0},
+      .mass = 1000*1000,
+      .position = (Vector2d) {GetRenderWidth()/2.0, GetRenderHeight()/2.0},
+      .velocity = (Vector2d) {0, 0},
+      .acceleration = (Vector2d) {0, 0},
     },
     {
       .mass = 10000,
-      .position = (Vector3d) {GetRenderWidth()/2.0 - 200, GetRenderHeight()/2.0, 0},
-      .velocity = (Vector3d) {0, vel*mass_ratio, 0},
-      .acceleration = (Vector3d) {0, 0, 0},
+      .position = (Vector2d) {GetRenderWidth()/2.0, GetRenderHeight()/2.0 - 50},
+      .velocity = (Vector2d) {-140, 0},
+      .acceleration = (Vector2d) {0, 0},
     },
     {
       .mass = 10000,
-      .position = (Vector3d) {GetRenderWidth()/2.0 + 200, GetRenderHeight()/2.0, 0},
-      .velocity = (Vector3d) {0, -vel*mass_ratio, 0},
-      .acceleration = (Vector3d) {0, 0, 0},
+      .position = (Vector2d) {GetRenderWidth()/2.0, GetRenderHeight()/2.0 + 100},
+      .velocity = (Vector2d) {140/sqrt(2.0), 0},
+      .acceleration = (Vector2d) {0, 0},
+    },
+    {
+      .mass = 10000,
+      .position = (Vector2d) {GetRenderWidth()/2.0, GetRenderHeight()/2.0 + 150},
+      .velocity = (Vector2d) {140/sqrtf(3.0), 0},
+      .acceleration = (Vector2d) {0, 0},
+    },
+    {
+      .mass = 10000,
+      .position = (Vector2d) {GetRenderWidth()/2.0, GetRenderHeight()/2.0 + 200},
+      .velocity = (Vector2d) {140/2.0, 0},
+      .acceleration = (Vector2d) {0, 0},
+    },
+    {
+      .mass = 10000,
+      .position = (Vector2d) {GetRenderWidth()/2.0, GetRenderHeight()/2.0 + 400},
+      .velocity = (Vector2d) {140/2.0/sqrtf(2.0), 0},
+      .acceleration = (Vector2d) {0, 0},
     },
   };
+  zero_momentum(particles, NUM_PARTICLES);
+
   long double acceleration_deltas[NUM_PARTICLES] = {0};
-  Vector3d force_vectors[NUM_PARTICLES][NUM_PARTICLES] = {0};
+  Vector2d force_vectors[NUM_PARTICLES][NUM_PARTICLES] = {0};
   long double radii[NUM_PARTICLES][NUM_PARTICLES] = {0};
 
-  Vector3d trail[NUM_PARTICLES][TRAIL_LENGTH] = {0};
+  Vector2d trail[NUM_PARTICLES][TRAIL_LENGTH] = {0};
 
   size_t counter = 0;
   while (!WindowShouldClose()) {
@@ -68,7 +102,7 @@ int main(void) {
     }
     
     for (size_t i=0; i<NUM_PARTICLES; i++) {
-      particles[i].acceleration = (Vector3d) {0};
+      particles[i].acceleration = (Vector2d) {0};
       for (size_t j=0; j<NUM_PARTICLES; j++) {
         if (i == j) continue;
         force_vectors[i][j] = VECTOR3D_DIFF(particles[j].position, particles[i].position);
@@ -92,7 +126,7 @@ int main(void) {
         DrawCircle(trail[part][i].x, trail[part][i].y, 2, GRAY);
         DrawCircle(trail[part][i].x, trail[part][i].y, 2, GRAY);
       }
-      DrawCircle(particles[part].position.x, particles[part].position.y, particles[part].mass/5000, WHITE);
+      DrawCircle(particles[part].position.x, particles[part].position.y, part_rad[part], WHITE);
     }
     EndDrawing();
   }
